f1c37742f31a0538280eceb3342472e24324221c
Infant Alex <vminfant@gmail.com>
2021-08-19 00:43:27 +0000

    updates to netflix script

--- netflix/netflix_pyspark.py
+++ netflix/netflix_pyspark.py
@@ -1,6 +1,8 @@
 """Script to transform the netflix data set from Kaggle."""
 from pyspark.sql import SparkSession
 
+DATA_PATH = "/home/jovyan/Git/pyspark_projects/data/netflix/"
+
 def main():
     """Transform the netflix data set."""
     
@@ -8,8 +10,15 @@ def main():
     spark = SparkSession.builder.master("local").appName("netflix").getOrCreate()
     print(spark)
 
+    # Define a schema for netflix data.
+    titles_schema = ("show_id STRING,type STRING,title STRING,director STRING,cast STRING,"
+                     "country STRING,date_added DATE,release_year INT,rating STRING,"
+                     "duration STRING,listed_in STRING,description STRING")
+
     # Load the data as a DataFrame.
-    inp_df = spark.read.
+    inp_df = spark.read.csv(DATA_PATH, schema=titles_schema, header=True)
+
+    print(inp_df.count())
 
 if __name__ == "__main__":
     main()
