4b635dcf0b1911d15e1edc521ddab4c6513d2340
Infant Alex <vminfant@gmail.com>
2021-08-20 00:07:01 +0000

    added transformation rules

---
+++ netflix/netflix.md
@@ -0,0 +1,11 @@
+# Transform netflix data set
+
+## Source
+
+| Source        | Link           |
+| ------------- |:-------------:|
+| Kaggle        | [netflix-shows](https://www.kaggle.com/shivamb/netflix-shows) |
+
+## Transformation rules
+
+* Convert the duration into seconds
--- netflix/netflix_pyspark.py
+++ netflix/netflix_pyspark.py
@@ -1,24 +1,37 @@
-"""Script to transform the netflix data set from Kaggle."""
+"""Script to transform the netflix data set from Kaggle.
+
+Usage example:
+    spark-submit netflix/netflix_pyspark.py -p data/netflix/
+"""
+import argparse
 from pyspark.sql import SparkSession
 
-DATA_PATH = "/home/jovyan/Git/pyspark_projects/data/netflix/"
+# Define a schema for netflix data.
+TITLES_SCHEMA = ("show_id STRING,type STRING,title STRING,director STRING,cast STRING,"
+                 "country STRING,date_added DATE,release_year INT,rating STRING,"
+                 "duration STRING,listed_in STRING,description STRING")
+
+def process_df(spark, inp_df):
+    """Process the input data frame and apply transformation rules."""
+    return inp_df
 
 def main():
     """Transform the netflix data set."""
-    
+    # Get arguments.
+    parser = argparse.ArgumentParser(description="Transform netflix data")
+    parser.add_argument("-p", "--path", type=str, required=True, help="Provide valid path")
+    args = parser.parse_args()
+
     # Create a spark session.
     spark = SparkSession.builder.master("local").appName("netflix").getOrCreate()
     print(spark)
 
-    # Define a schema for netflix data.
-    titles_schema = ("show_id STRING,type STRING,title STRING,director STRING,cast STRING,"
-                     "country STRING,date_added DATE,release_year INT,rating STRING,"
-                     "duration STRING,listed_in STRING,description STRING")
-
     # Load the data as a DataFrame.
-    inp_df = spark.read.csv(DATA_PATH, schema=titles_schema, header=True)
+    # Some of the description and titles are produced in multiple lines.
+    inp_df = spark.read.csv(args.path, schema=TITLES_SCHEMA, header=True, multiLine=True)
 
-    print(inp_df.count())
+    # Process the data frame
+    process_df(spark, inp_df)
 
 if __name__ == "__main__":
     main()
